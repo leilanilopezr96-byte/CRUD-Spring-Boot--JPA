7486d69ed9fc768a6b4bd71d42141f9c6c693266
Leilani Sofia López Rivera <leilanilopezr96@gmail.com>
2026-05-07 12:41:32 -0600

    Agrego archivos de proyecto

---
+++ jpa/src/main/java/org/generation/products/service/ProductService.java
@@ -0,0 +1,25 @@
+package org.generation.products.service;
+
+import org.generation.products.model.Product;
+import org.generation.products.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
