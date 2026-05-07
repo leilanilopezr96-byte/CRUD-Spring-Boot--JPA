d5c18e0ba15a7c8674f0de971ae3c3ba7f37a505
Leilani Sofia López Rivera <leilanilopezr96@gmail.com>
2026-05-07 12:43:01 -0600

    Delete jpa directory

--- jpa/src/main/java/org/generation/products/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.generation.products.repository;
-
-import org.generation.products.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
